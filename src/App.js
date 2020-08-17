906352e0dcde900f7e158fed73f40a33c6296e70
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2020-08-17 22:55:02 +0300

    Change App function to class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Phrase of the day</h1>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Phrase of the day</h1>
+        </header>
+      </div>
+    );
+  }
+  }
+ 
 
 export default App;
